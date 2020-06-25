7217ea43da7cfb95eff158f8956176b0f0c58141
Rick Nijhuis <rick.nijhuis@sycade.com>
2020-06-25 20:39:33 +0200

    init

---
+++ build/precache-manifest.a31409bba9e205330bc0044b11ad56f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "840712fcda997e3d73ca77a2efb56203",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "ebf84e86a99460ead9ae",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/my-app/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebf84e86a99460ead9ae",
+    "url": "/my-app/static/js/main.8ce947f1.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
